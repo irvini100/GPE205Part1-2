f7c1498d79986872df2556c82c8a915aafeaff85
Irvin Ingram <82079816+irvini100@users.noreply.github.com>
2021-09-13 21:10:22 -0400

    Add files via upload
    Here's my second try of my first assignment of GPE205.

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : Controller
+{
+
+    public enum ControlType { WASD, IJKL  };
+    public ControlType controls;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (controls == ControlType.WASD)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {  //Move forward
+                Pawn.mover.MoveForward();
+            }
+        }
+        else
+        {
+            if (controls == ControlType.IJKL)
+            {
+
+            }
+        }
+	}
+}
---
+++ Assets/Scripts/TankData.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankData : MonoBehaviour {
+
+    public TankMover mover;
+    public TankShooter shooter;
+    //Variables for our tanks in game
+    public float speed;
+    public float turnSpeed;
+
+	// Use this for initialization
+	void Start () {
+        //Load the mover
+        mover = this.gameObject.GetComponent<TankMover>();
+        //Load the shooter.
+        shooter = this.gameObject.GetComponent<TankShooter>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
